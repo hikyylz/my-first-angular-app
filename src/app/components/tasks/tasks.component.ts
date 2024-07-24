1e80178851f8b418d048f96487d09a87aae96963
Kaan Yıldız <70718902+hikyylz@users.noreply.github.com>
2024-07-24 13:31:55 +0300

    tasks viewed in the screen

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-tasks'; // iki üst folder a gitme yolu.
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './tasks.component.html',
+  styleUrl: './tasks.component.css'
+})
+
+export class TasksComponent implements OnInit{
+  tasks: Task[] = TASKS;
+  constructor(){}
+  ngOnInit(): void{
+
+  }
+}
